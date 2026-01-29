bb75b80267248b57b8a1c7b3292d5f3bdb482755
Gábor Pór <gabor.por@lombiq.com>
2026-01-29 19:02:03 +0100

    Fixing trackInfo formatting

--- common/trackInfo.js
+++ common/trackInfo.js
@@ -1,21 +1,62 @@
 module.exports = {
-  Bahrain:
-  {
-      countryCode: 'BH',
-      displayName: 'Bahrain'
+  Bahrain: {
+    countryCode: 'BH',
+    displayName: 'Bahrain',
   },
-  'Circuit Of The Americas': { countryCode: 'US', displayName: 'Circuit Of The Americas' },
-  Fuji: { countryCode: 'JP', displayName: 'Fuji' },
-  Fuji_Cl: { countryCode: 'JP', displayName: 'Fuji_Cl' },
-  Imola: { countryCode: 'IT', displayName: 'Imola' },
-  Interlagos: { countryCode: 'BR', displayName: 'Interlagos' },
-  Lemans: { countryCode: 'FR', displayName: 'Lemans' },
-  Monza: { countryCode: 'IT', displayName: 'Monza' },
-  Portimao: { countryCode: 'PT', displayName: 'Portimao' },
-  Qatar: { countryCode: 'QA', displayName: 'Qatar' },
-  Sebring: { countryCode: 'US', displayName: 'Sebring' },
-  Silverstone: { countryCode: 'GB', displayName: 'Silverstone' },
-  Spa: { countryCode: 'BE', displayName: 'Spa' },
-  Spa_Endce: { countryCode: 'BE', displayName: 'Spa_Endce' },
-  Paulricard: { countryCode: 'FR', displayName: 'Paulricard' },
+  'Circuit Of The Americas': {
+    countryCode: 'US',
+    displayName: 'Circuit Of The Americas',
+  },
+  Fuji: {
+    countryCode: 'JP',
+    displayName: 'Fuji',
+  },
+  Fuji_Cl: {
+    countryCode: 'JP',
+    displayName: 'Fuji_Cl',
+  },
+  Imola: {
+    countryCode: 'IT',
+    displayName: 'Imola',
+  },
+  Interlagos: {
+    countryCode: 'BR',
+    displayName: 'Interlagos',
+  },
+  Lemans: {
+    countryCode: 'FR',
+    displayName: 'Lemans',
+  },
+  Monza: {
+    countryCode: 'IT',
+    displayName: 'Monza',
+  },
+  Portimao: {
+    countryCode: 'PT',
+    displayName: 'Portimao',
+  },
+  Qatar: {
+    countryCode: 'QA',
+    displayName: 'Qatar',
+  },
+  Sebring: {
+    countryCode: 'US',
+    displayName: 'Sebring',
+  },
+  Silverstone: {
+    countryCode: 'GB',
+    displayName: 'Silverstone',
+  },
+  Spa: {
+    countryCode: 'BE',
+    displayName: 'Spa',
+  },
+  Spa_Endce: {
+    countryCode: 'BE',
+    displayName: 'Spa_Endce',
+  },
+  Paulricard: {
+    countryCode: 'FR',
+    displayName: 'Paulricard',
+  },
 };
